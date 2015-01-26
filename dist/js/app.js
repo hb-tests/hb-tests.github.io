2436444f4ca6b928f69cb12fe6a5bf86a4e1adc2
Adam Daigian <adam.daigian@gmail.com>
2015-01-26 13:21:28 -0800

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+
+  //mixpanel track
+  $('[track]').click(function () {
+    mixpanel.track($(this).attr("track"))  
+  });
+
+  //signup
+  $('#home-sign-up').click(function () { 
+    $('#home-sign-up').hide()
+    // $('#google-signup').hide()
+    // $('#email-signup').hide()
+    // $('.divider').hide()
+    $('#lp-registration').show()
+  });
+
+    $('#home-sign-up-footer').click(function () { 
+    $('#home-sign-up').hide()
+    // $('#google-signup').hide()
+    // $('#email-signup').hide()
+    // $('.divider').hide()
+    $('#lp-registration').show()
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
+  //testimonials section
+  // $(".data-section-title").click(function() {
+  //     obj = $(this)
+  //     data_toggable_family = obj.parent().data("toggable-target-family");
+  //     console.log($(this))
+  //     data_toggable_name = obj.data("toggable-target-name");
+  //     obj.siblings().removeClass("active");
+  //     obj.addClass('active');
+
+  //     $("[data-toggable-family='"+data_toggable_family+"']").children().hide();
+  //     $("[data-toggable-family='"+data_toggable_family+"']").children("[data-toggable-name='"+ data_toggable_name+"']").show();
+  // });
+
+  //testimonials section
+  $('#data-section-1').click(function () {
+      $('#avatar-1').addClass('avatar-active')
+      $('#avatar-2').removeClass('avatar-active')
+      $('#avatar-3').removeClass('avatar-active')
+      $('#avatar-4').removeClass('avatar-active')
+      $('#testimonial-1').addClass('active')
+      $('#testimonial-2').removeClass('active')
+      $('#testimonial-3').removeClass('active')
+      $('#testimonial-4').removeClass('active')
+      mixpanel.track('stationary')
+  });
+  $('#data-section-2').click(function () {
+      $('#avatar-1').removeClass('avatar-active')
+      $('#avatar-2').addClass('avatar-active')
+      $('#avatar-3').removeClass('avatar-active')
+      $('#avatar-4').removeClass('avatar-active')
+      $('#testimonial-1').removeClass('active')
+      $('#testimonial-2').addClass('active')
+      $('#testimonial-3').removeClass('active')
+      $('#testimonial-4').removeClass('active')
+      mixpanel.track('stationary')
+  });
+  $('#data-section-3').click(function () {
+      $('#avatar-1').removeClass('avatar-active')
+      $('#avatar-2').removeClass('avatar-active')
+      $('#avatar-3').addClass('avatar-active')
+      $('#avatar-4').removeClass('avatar-active')
+      $('#testimonial-1').removeClass('active')
+      $('#testimonial-2').removeClass('active')
+      $('#testimonial-3').addClass('active')
+      $('#testimonial-4').removeClass('active')
+      mixpanel.track('stationary')
+  });
+  $('#data-section-4').click(function () {
+      $('#avatar-1').removeClass('avatar-active')
+      $('#avatar-2').removeClass('avatar-active')
+      $('#avatar-3').removeClass('avatar-active')
+      $('#avatar-4').addClass('avatar-active')
+      $('#testimonial-1').removeClass('active')
+      $('#testimonial-2').removeClass('active')
+      $('#testimonial-3').removeClass('active')
+      $('#testimonial-4').addClass('active')
+      mixpanel.track('stationary')
+  });
+
+  jQuery(function() {
+    var menu = jQuery(".navbar.navbar-default.navbar-fixed-top");
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 15) {
+         console.log('hello')
+         menu.addClass("navbar-fill");
+    } else {
+        menu.removeClass("navbar-fill");
+      }
+    });
+  });
+
+});
