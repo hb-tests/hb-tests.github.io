8128d1d961cc76840dc744270cae85be1cacb26b
Adam <adam.daigian@gmail.com>
2016-06-04 21:35:50 -0700

    done with lp

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,45 +10,39 @@ $(document).ready(function() {
     mixpanel.track('~page scrolled');
   });
 
-
   var nominatorFormHandlerURL = 'https://go.pardot.com/l/156821/2016-06-03/39g9f';
-  console.log('doo doo')
-  $('#submit_btn').on('click', function () {
-    console.log('herro')
-
+    $('#nominator_btn').on('click', function () {
     $('body').append("<iframe id='pardot-nominator-handler' height='0' width='0' style='position:absolute; left:-100000px; top:-100000px' src='" + nominatorFormHandlerURL + "?firstname=" + $('#nominator_first_name').val() + "&lastname=" + $('#nominator_last_name').val() + "&email=" + $('#nominator_email').val() + "'></iframe>");
-    console.log('doo doo')
     $('#pardot-nominator-handler').submit();
-    console.log('submitted')
+    $('#nominator_container').hide()
+    $('#nominee_container').show()
   });
 
 
-    function sendNominee () {
-    var pardotFormHandlerURL = 'https://go.pardot.com/l/156821/2016-06-03/39g9h';
+  var nomineeFormHandlerURL = 'https://go.pardot.com/l/156821/2016-06-03/39g9h';
+  $('#nominee_btn').on('click', function () {
+    $('#nominee_success').hide()
+    $('body').append("<iframe id='pardot-nominator-handler' height='0' width='0' style='position:absolute; left:-100000px; top:-100000px' src='" + nomineeFormHandlerURL + "?firstname=" + $('#nominee_first_name').val() + "&lastname=" + $('#nominee_last_name').val() + "&email=" + $('#nominee_email').val() + "&referredby=" + $('#nominator_first_name').val() + " " + $('#nominator_last_name').val() + "&referringmessage=" + $('#nominee_message').val() + "'></iframe>");
+    $('#pardot-nominee-handler').submit();
+    setTimeout(function(){
+      $('#nominee_container form :input').val("")
+      $('#nominee_success').show()
+    }, 1000);
+  });
 
-    $('#contract_btn').on('click', function () {
-
-      $('body').append("<iframe id='pardot-nominator-handler' height='0' width='0' style='position:absolute; left:-100000px; top:-100000px' src='" + pardotFormHandlerURL + "?email=" + $('#prospectEmail').val() + "'></iframe>");
-      console.log('doo doo')
-      $('#pardot-nominee-handler').submit();
-      console.log('submitted')
+  jQuery(function() {
+    var menu = jQuery(".navbar.navbar-default.navbar-fixed-top");
+    var btn = $('.btn-nav')
+    jQuery(window).scroll(function() {    
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 10) {
+         menu.addClass("navbar-fill");
+         btn.show();
+    } else {
+        menu.removeClass("navbar-fill");
+        btn.hide();
+      }
     });
-  }
-
-  window.onload
-
-
-  // jQuery(function() {
-  //   var menu = jQuery(".navbar.navbar-default.navbar-fixed-top");
-  //   jQuery(window).scroll(function() {    
-  //       var scroll = jQuery(window).scrollTop();
-  //       if (scroll >= 15) {
-  //        console.log('hello')
-  //        menu.addClass("navbar-fill");
-  //   } else {
-  //       menu.removeClass("navbar-fill");
-  //     }
-  //   });
-  // });
+  });
 
 });
